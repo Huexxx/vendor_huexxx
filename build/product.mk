ccc85877786c095accd0e77ac57cada2322e1c6d
Huexxx <huexxx@gmail.com>
2021-11-23 12:33:24 +0100

    Cgapps: change to own gapps repo

--- build/product.mk
+++ build/product.mk
@@ -91,13 +91,12 @@ PRODUCT_PACKAGES += \
 # Gapps
 ifeq ($(WITH_GMS),true)
 
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product, vendor/gapps/gapps.mk)
 
 # SetupWizard and Google Assistant properties
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.setupwizard.rotation_locked=true \
     setupwizard.theme=glif_v3_light \
-    ro.opa.eligible_device=true \
 
 endif
 
