588002c10205ee97c28e51e9dd79c20bacccafac
Huexxx <huexxx@gmail.com>
2022-02-20 16:03:19 +0100

    soong: Add LIBBINDER_HAS_THE_SAME_HEADER flag

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -61,7 +61,8 @@ SOONG_CONFIG_aospQcomVars += \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
     uses_qti_camera_device \
-    needs_camera_boottime_timestamp
+    needs_camera_boottime_timestamp \
+    libbinder_has_the_same_header
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -90,6 +91,7 @@ SOONG_CONFIG_aospQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_
 SOONG_CONFIG_aospQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_aospQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_aospQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_aospQcomVars_libbinder_has_the_same_header := $(TARGET_SAME_BINDER_HEADER_WITH_SYSTEM)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
