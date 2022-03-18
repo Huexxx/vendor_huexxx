5a10131d0f01e186c2203301b2c8a2d149c2d3fe
Huexxx <huexxx@gmail.com>
2022-03-18 18:56:41 +0100

    Add PACKAGE_BUILD_TYPE info to OTAPACKAGE

--- build/tools/buildinfo_aex.sh
+++ build/tools/buildinfo_aex.sh
@@ -5,7 +5,7 @@ echo "ro.extended.version=$EXTENDED_VERSION"
 echo "ro.extended.releasetype=$EXTENDED_BUILD_TYPE"
 echo "ro.modversion=AospExtended-$EXTENDED_VERSION-`$DATE +%Y%m%d-%H%M`-$EXTENDED_BUILD_TYPE"
 echo "ro.extended.display.version=AospExtended-$EXTENDED_VERSION-$EXTENDED_BUILD_TYPE"
-echo "ro.extended.fingerprint=AospExtended/$EXTENDED_VERSION/$PLATFORM_VERSION/$AOSP_DEVICE/`$DATE +%Y%m%d-%H%M`"
+echo "ro.extended.fingerprint=AospExtended/$EXTENDED_VERSION/$PLATFORM_VERSION/$AOSP_DEVICE/$PACKAGE_BUILD_TYPE/`$DATE +%Y%m%d-%H%M`"
 echo "ro.extended.build_date_utc=`$DATE +%s`"
 
 echo "# end AospExtended build properties"
