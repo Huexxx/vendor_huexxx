4a4f43ec74d3776d3fdd9d24481f488f754a73e4
Huexxx <huexxx@gmail.com>
2022-03-22 21:06:34 +0100

    Enable ADB authentication only for user target

--- config/common.mk
+++ config/common.mk
@@ -2,12 +2,12 @@ PRODUCT_BRAND ?= aosp
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-# Disable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
-else
+ifeq ($(TARGET_BUILD_VARIANT),user)
 # Enable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+else
+# Disable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
 endif
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
