3d7aabde1feb96494e5d32d39c163454ef2a5958
nift4 <nift4@protonmail.com>
2024-08-14 16:24:35 +0800

    config: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I9c0aad6ee175413ccb9792b036340fbdfdc547e6
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -31,11 +31,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/bliss/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/bliss/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/bliss/prebuilt/common/bin/50-bliss.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-bliss.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-bliss.sh
+    vendor/bliss/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
